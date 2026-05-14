e6e7618f23f09fb2f430929ee267b78f3f83884b
PPvuohi <61870469+PPvuohi@users.noreply.github.com>
2026-05-14 17:26:27 +0000

    Päivitetty konstruktorit

--- Urheilu1/Javascript3.js
+++ Urheilu1/Javascript3.js
@@ -1,4 +1,10 @@
 class Henkilo {
+  /**
+   * @param {any} etunimet
+   * @param {any} sukunimi
+   * @param {any} kutsumanimi
+   * @param {any} syntymavuosi
+   */
   constructor(etunimet, sukunimi, kutsumanimi, syntymavuosi) {
     this.etunimet = etunimet;
     this.sukunimi = sukunimi;
@@ -8,12 +14,22 @@ class Henkilo {
 }
 
 class Urheilija extends Henkilo {
-  constructor(etunimet, sukunimi, kutsumanimi, syntymavuosi) {
+  /**
+   * @param {string} etunimet
+   * @param {string} sukunimi
+   * @param {string} kutsumanimi
+   * @param {number} syntymavuosi
+   * @param {string} kuva
+   * @param {number} paino
+   * @param {string} laji
+   * @param {{ vuosi: number; tapahtuma: string; joukkue: string; tulos: string; }[]} saavutukset
+   */
+  constructor(etunimet, sukunimi, kutsumanimi, syntymavuosi, kuva, paino, laji, saavutukset) {
     super(etunimet, sukunimi, kutsumanimi, syntymavuosi);
-    this._kuva = null;
-    this._paino = null;
-    this._laji = null;
-    this._saavutukset = [];
+    this._kuva = kuva;
+    this._paino = paino;
+    this._laji = laji;
+    this._saavutukset = saavutukset;
   }
   get kuva() {
     return this._kuva;
@@ -44,14 +60,14 @@ class Urheilija extends Henkilo {
   }
 }
 
-let urheilija1 = new Urheilija("Hessu", "Hopo", "Hessu", 1932);
-urheilija1.kuva =
-  "https://www.artnet.com/WebServices/images/ll19843lldmBzJFg3okR3CfDrCWvaHBOcPneG/walt-disney-studios-sport-goofy-in-soccermania-scrooge-mcduck-production-cel-(walt-disney,-1987).jpg";
-urheilija1.paino = 60;
-urheilija1.laji = "jalkapallo";
-urheilija1.saavutukset.concat({
+let urheilija1 = new Urheilija("Hessu", "Hopo", "Hessu", 1932, 
+  "https://www.artnet.com/WebServices/images/ll19843lldmBzJFg3okR3CfDrCWvaHBOcPneG/walt-disney-studios-sport-goofy-in-soccermania-scrooge-mcduck-production-cel-(walt-disney,-1987).jpg",
+  60,
+  "jalkapallo",
+  [{
   vuosi: 1987,
   tapahtuma: "Soccermania",
   joukkue: "McDuck Greenbacks",
   tulos: "voitto 10-0",
-});
+  }]);
+console.log(urheilija1);
